7fe6492ec033c736c9cf8eaebdafb89ab90fd8bf
celeste emery <beecroncher@gmail.com>
2024-02-09 14:18:36 -0600

    install interact.js and make snapping elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 // Video4Ever Starter Code
 // Dr. Miller
 // Start your React app using npm start while in the client directory
-
+import interact from 'interactjs';
 import './App.css';
 import React, { useState, useEffect } from "react";
 
@@ -28,6 +28,61 @@ import React, { useState, useEffect } from "react";
     });
     return response.json(); // parses JSON response into native JavaScript objects
 }
+/* */
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.snap({
+        targets: [
+          interact.snappers.grid({ x: 30, y: 30 })
+        ],
+        range: Infinity,
+        relativePoints: [ { x: 0, y: 0 } ]
+      }),
+      interact.modifiers.restrict({
+        restriction: 'parent',
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+        endOnly: true
+      })
+    ],
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener
+
+      //changes element text to "element moved xyz pixels idk"
+      // call this function on every dragend event
+      //end (event) {
+        /*
+        var textEl = event.target.querySelector('p')
+
+        textEl && (textEl.textContent =
+          'moved a distance of ' +
+          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px')
+            */
+      //}
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target;
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+}
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
 
 function App() {
   // Use this variable whenever you want to connect to the Node.js server
@@ -55,8 +110,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>{message}</h1>
-
+      
+    
+        <div id="drag-1" class="draggable">
+          <p> 1 </p>
+        </div>
+        <div id="drag-2" class="draggable">
+          <p> 2 </p>
+        </div>
+        <div id="drag-2" class="draggable">
+          <p> 3 </p>
+        </div>
+        <div id="drag-2" class="draggable">
+          <p> 4 </p>
+        </div>
+        <div id="drag-2" class="draggable">
+          <p> 5 </p>
+        </div>
+        <div id="drag-2" class="draggable">
+          <p> 6 </p>
+        </div>
+        
       </header>
       
     </div>
