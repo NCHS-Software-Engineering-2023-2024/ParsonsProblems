81f60c1c3f8de2b6de2e39010a0e7a5db2ae830a
celeste emery <beecroncher@gmail.com>
2024-02-14 14:23:06 -0600

    try to take a look at draggable

--- client/src/App.js
+++ client/src/App.js
@@ -52,7 +52,7 @@ interact('.draggable')
     listeners: {
       // call this function on every dragmove event
       move: dragMoveListener
-
+      
       //changes element text to "element moved xyz pixels idk"
       // call this function on every dragend event
       //end (event) {
@@ -70,27 +70,13 @@ interact('.draggable')
   })
   .resizable({
     // resize from all edges and corners
-    edges: { left: true, right: true, bottom: true, top: true },
 
     listeners: {
       move (event) {
         var target = event.target
-        var x = (parseFloat(target.getAttribute('data-x')) || 0)
-        var y = (parseFloat(target.getAttribute('data-y')) || 0)
 
         // update the element's style
         target.style.width = event.rect.width + 'px'
-        target.style.height = event.rect.height + 'px'
-
-        // translate when resizing from top or left edges
-        x += event.deltaRect.left
-        y += event.deltaRect.top
-
-        target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
-
-        target.setAttribute('data-x', x)
-        target.setAttribute('data-y', y)
-        target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
       }
     },
     modifiers: [
@@ -105,7 +91,6 @@ interact('.draggable')
       })
     ],
 
-    inertia: true
   })
 
 function dragMoveListener (event) {
