5f7265b2e0e003d8e44de51bfb48a4f67e939aab
celeste emery <beecroncher@gmail.com>
2024-02-23 14:14:14 -0600

    the blocks now scoot over with dragover

--- client/src/dnd-container.js
+++ client/src/dnd-container.js
@@ -1,20 +1,25 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { DndItem } from "./dnd-item.js"
-import { update } from "immutability-helper"
+import React, { useState, useEffect } from "react";
+import update from "immutability-helper"
 
 export const DndContainer = () => {
     const [items, setItems] = useState([
         {
             id: 1,
             name: 'firstasdjkahsaskakskdjasjkdjkakjsssssssssssssskjskkdasd aasdasdakslk',
+            positionx: null,
+            positiony: null,
         },
         {
             id: 2,
             name: 'second',
+            positionx: null,
+            positiony: null,
         },
         {
             id: 3,
             name: 'third',
+            positionx: null,
+            positiony: null,
         },
     ]);
     const [draggingItem, setDraggingItem] = useState(null);
@@ -24,12 +29,25 @@ export const DndContainer = () => {
         e.dataTransfer.setData('text/plain', '');
     };
 
+
     const handleDragEnd = () => {
         setDraggingItem(null);
     };
 
-    const handleDragOver = (e) => {
+    const handleDragOver = (e, targetItem) => {
         e.preventDefault();
+        
+        const currentIndex = items.indexOf(draggingItem);
+        const targetIndex = items.indexOf(targetItem);
+        if (currentIndex !== -1 && targetIndex !== -1) {
+            setItems((prevItems) =>
+                update(prevItems, {
+                    $splice: [
+                        [currentIndex, 1],
+                        [targetIndex, 0, draggingItem],
+                    ]
+            }));
+        }
     };
 
     const handleDrop = (e, targetItem) => {
@@ -49,47 +67,34 @@ export const DndContainer = () => {
         }
     };
 
- 
-    const moveItem = useCallback((dragIndex, hoverIndex) => {
-        setItems((prevItems) =>
-          update(prevItems, {
-            $splice: [
-              [dragIndex, 1],
-              [hoverIndex, 0, prevItems[dragIndex]],
-            ],
-          })
-        );
-      }, []);
-    const renderItem = useCallback((item, index, classN, dragStartN, onDropN) => {
-        return (
-            <>
-                <DndItem
-                    key={item.id}
-                    index={index}
-                    id={item.id}
-                    text={card.name}
-                    moveItem={moveItem}
-                    className={classN}
-                    onDragStart={dragStartN}
-                    onDragEnd={handleDragEnd}
-                    onDragOver={handleDragOver}
-                    onDrop={onDropN}
-                />
-            </>
-        );
-    }, []);
-
+    useEffect(() => {
+    const noSelectElements =
+    document.querySelectorAll(".item.details");
+    
+    noSelectElements.forEach((element) => {
+        element.style.mozUserSelect = "none";
+        element.style.msUserSelect = "none";
+        element.style.userSelect = "none";
+    });
+})
     return (
         <>
             <div className="sortable-list">
                 {items.map((item, index) => (
-                    renderItem(
-                        item, 
-                        index, 
-                        `item ${item === draggingItem ?
-                        'dragging' : ''}`,
-                        (e) => handleDragStart(e, item),
-                        (e) => handleDrop(e, item)),
+                <>
+                    <div class="details"
+                        key={item.id}
+                        index={index}
+                        id={item.id}
+                        className={`item ${item === draggingItem ?
+                            'dragging' : ''}`}
+                        onDragStart={(e) => handleDragStart(e, item)}
+                        onDragEnd={handleDragEnd}
+                        onDragOver={(e) => handleDragOver(e, item)}
+                        onDrop={(e) => handleDrop(e, item)}   >
+                            {item.name}
+                    </div>
+                </> 
                 ))}
             </div>
         </>
