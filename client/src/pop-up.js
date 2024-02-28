8231855237b2df909ff72fa2e8355a54f66b854d
SethTan1 <111527639+SethTan1@users.noreply.github.com>
2024-02-27 20:14:34 -0600

    Squashed commit of the following:
    commit e3b0a68aabb4f166d961ef20c86785964c269a75
    Merge: 2d41a1124 cf7e68392
    Author: SethTan1 <sitan@stu.naperville203.org>
    Date:   Tue Feb 27 14:19:11 2024 -0600
    
        Merge branch 'homepage' into upload-pop-up
    
    commit 2d41a1124f4e2b165597f923f96dcf232a0a0cc3
    Author: SethTan1 <sitan@stu.naperville203.org>
    Date:   Thu Feb 22 14:20:14 2024 -0600
    
        comments on popup.js
    
    commit df5cede9d26ffe150d76d2c548e4730e8084aecf
    Author: SethTan1 <sitan@stu.naperville203.org>
    Date:   Wed Feb 21 14:09:56 2024 -0600
    
        changed button to match other buttons, make indented button call function before merging
    
    commit ba617f98ec1ef2a8ab7971d82fde56385c27fac3
    Author: SethTan1 <111527639+SethTan1@users.noreply.github.com>
    Date:   Tue Feb 20 17:19:54 2024 -0600
    
        finished pop-up for file upload; imported bootstrap css; need to upload file to server
    
    commit 24174fdaa3fa7f7a8b5cf691e86ab6b2bffba2f6
    Author: SethTan1 <sitan@stu.naperville203.org>
    Date:   Tue Feb 20 14:18:33 2024 -0600
    
        starting to format upload button
    
    commit 1c77691a766b82e0d4c972d8cda8c261a35e9c0d
    Author: SethTan1 <111527639+SethTan1@users.noreply.github.com>
    Date:   Thu Feb 15 16:50:52 2024 -0600
    
        pop-up; no css yet
    
    commit 678548aeb09a3bb6d58d5281ddf9f42bb522604b
    Author: SethTan1 <sitan@stu.naperville203.org>
    Date:   Thu Feb 15 14:19:27 2024 -0600
    
        created other file for pop up
    
    commit c3d8f91c69ca8aad8d7583700236f5ca407ca56c
    Author: SethTan1 <sitan@stu.naperville203.org>
    Date:   Wed Feb 14 14:22:42 2024 -0600
    
        changed popup method to react
    
    commit 3c074194847874e61292abd65fbacbe99d4bef37
    Author: SethTan1 <sitan@stu.naperville203.org>
    Date:   Tue Feb 13 14:19:23 2024 -0600
    
        formatting upload button

---
+++ client/src/pop-up.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+function PopUp() {
+  //hooks - makes the popup 'appear' 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <button variant="primary" onClick={handleShow}>
+        Upload
+      </button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        
+        centered = {true}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Upload</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/*file upload from bootstrap*/}
+          <Form.Group controlId="formFile" className="mb-3" >
+            <Form.Label>Choose a File: 
+            </Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+                        <Button variant="primary">Upload</Button> {/*call function to write uploaded file to database*/}
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default PopUp;
