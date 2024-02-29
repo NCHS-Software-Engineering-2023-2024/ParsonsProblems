0a83d9d05f96a29995f74d7830aea28ef0b67619
celeste emery <beecroncher@gmail.com>
2024-02-29 14:13:33 -0600

    test problem is now in app.js instead of dnd-container

--- client/src/App.js
+++ client/src/App.js
@@ -43,6 +43,26 @@ function App() {
     // The initial state of the message is an empty string. When the variable is changed, it changes everywhere it is used.
     // This is referred to as a state hook
     const [message, setMessage] = useState("");
+    const [file, setFile] = useState([
+        {
+            id: 1,
+            name: 'firstasdjkahsaskakskdjasjkdjkakjsssssssssssssskjskkdasd aasdasdakslk',
+            positionx: null,
+            positiony: null,
+        },
+        {
+            id: 2,
+            name: 'second',
+            positionx: null,
+            positiony: null,
+        },
+        {
+            id: 3,
+            name: 'third',
+            positionx: null,
+            positiony: null,
+        },
+    ])
 
     // useEffect will run when the app loads
     // This is referred to as an effect hook
@@ -57,7 +77,8 @@ function App() {
     <div className="App">
       
       <body className="App-body">
-      < DndContainer />
+      < DndContainer file={file} 
+      />
         <div class = "container-md border">Content</div>
 
           
