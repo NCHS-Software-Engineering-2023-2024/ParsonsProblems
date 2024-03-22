6a2e7a7912897018de5a8029eded133213a87bbc
celeste emery <88988526+c-emery-0@users.noreply.github.com>
2024-03-22 18:51:10 +0000

    bugfixing

--- client/src/dnd-container.js
+++ client/src/dnd-container.js
@@ -17,6 +17,8 @@ import { CSS } from "@dnd-kit/utilities";
 import React, { useEffect, useState } from "react";
 import { fileContext } from "./fileContext.js";
 import { snapGridModifier } from "./snapGridModifier.ts";
+import { Grade, Shuffle } from "./dnd-grading"
+import "./index.css";
 
 function SortableItem(props) {
   const {
@@ -47,9 +49,11 @@ function SortableItem(props) {
 }
 export const DndContainer = () => {
   const {file, setFile} = React.useContext(fileContext);
-  const [items, setitems] = useState(file);
+  const [items, setItems] = useState(Shuffle(file));
+  const [activeId, setActiveId] = useState(null);
+  const [grade, setGrade] = useState(Grade(items));
   
-  useEffect(() => setitems(file), [file]);
+  useEffect(() => setItems(file), [file]);
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -57,7 +61,6 @@ export const DndContainer = () => {
     })
   );
   
-  const [activeId, setActiveId] = useState(null);
 
   function handleDragStart(event) {
     setActiveId(event.active.id);
@@ -67,7 +70,7 @@ export const DndContainer = () => {
     const { active, over } = event;
 
     if (activeId !== over.id) {
-      setitems((items) => {
+      setItems((items) => {
         const oldIndex = items.indexOf(activeId);
         const newIndex = items.indexOf(over.id);
 
